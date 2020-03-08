8a57969a9c72d55b93487d409075798f2a99ab35
aubique <ubicquita@gmail.com>
2020-03-08 19:16:02 +0100

    Load and access the shared in-memory H2 DB

--- spring-yatl/src/main/java/dev/aubique/yatl/YatlApplication.java
+++ spring-yatl/src/main/java/dev/aubique/yatl/YatlApplication.java
@@ -1,16 +1,23 @@
 package dev.aubique.yatl;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude = {JacksonAutoConfiguration.class})
+import java.sql.SQLException;
+
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class YatlApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(YatlApplication.class, args);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 
 }
