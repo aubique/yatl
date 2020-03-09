4741e60e331c7e107686a947a3bd17855c0eb1b9
aubique <ubicquita@gmail.com>
2020-03-09 12:26:57 +0100

    Integrate mock tests and log4j2 into the project
    - Import log4j2
    - Mock controllers for integration test
    - Add a boolean field to TodoEntity
    - Fix StackOverFlow in one of the service methods

--- spring-yatl/src/main/java/dev/aubique/yatl/model/Todo.java
+++ spring-yatl/src/main/java/dev/aubique/yatl/model/Todo.java
@@ -1,12 +1,15 @@
 package dev.aubique.yatl.model;
 
 import com.google.gson.annotations.Expose;
-import lombok.Data;
-import lombok.ToString;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "todo")
 public class Todo {
@@ -16,16 +19,20 @@ public class Todo {
     @Expose
     private Long id;
 
-    @ToString.Exclude
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+    @Expose
+    @Column(name = "completed", nullable = false)
+    private Boolean completed;
 
     @Expose
-    @Column(name = "title", length = 32)
+    @Column(name = "title", length = 32, nullable = false)
     private String title;
 
     @Expose
     @Column(name = "description", length = 255)
     private String description;
+
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
